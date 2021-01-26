e2578a6f51f03f504f6d99150d7ca5129a26f291
Humzabj <humza-b-jay@hotmail.com>
2021-01-26 15:56:24 +0500

    Adding dynamo db api

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3001
+var AWS = require('aws-sdk');
+let awsConfig = require('./aws-config.js');
 
-app.get('/', (req, res) => res.send('Mail Archiver backend'))
+AWS.config.update(awsConfig);
+let documentClient = new AWS.DynamoDB.DocumentClient();
+
+app.get('/', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    var params = {
+        TableName: "email_archive",
+    }
+    documentClient.scan(params, function (err, data) {
+        if (err) {
+            console.log(JSON.stringify(err))
+            res.send(JSON.stringify(err))
+        } else {
+            console.log(JSON.stringify(data))
+            res.send(JSON.stringify(data))
+        }
+    })
+})
 
 app.listen(port)
